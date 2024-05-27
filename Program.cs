83a7e408737d205d7fa923e8712be02f72792373
tristanrosel <roseltristan6@gmail.com>
2024-05-27 19:52:06 +0800

    refactored

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 public class Kata
 {
@@ -23,18 +22,15 @@ public class Kata
 
     private static int GetBit(int value, int plane)
     {
-        // Shift the value right by the plane number to extract the specified bit
         return (value >> plane) & 1;
     }
 
     public static void Main(string[] args)
     {
-        // Example usage
         int[][] image = new int[][] { new int[] { 0, 63 }, new int[] { 127, 255 } };
         int plane = 3;
         int[][] result = BitPlane(image, plane);
 
-        // Output the result
         Console.WriteLine("Bit plane " + plane + ":");
         foreach (var row in result)
         {
